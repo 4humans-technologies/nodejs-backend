7d30487fbae88f22fa34623e9c3fce39b643c574
Rohit kumar <rohitjadav1729@gmail.com>
2021-08-15 15:50:51 +0530

    first commit

---
+++ socket.js
@@ -0,0 +1,13 @@
+let io;
+module.exports = {
+    init: (serverInstance, options = null) => {
+        io = require('socket.io')(serverInstance, options)
+        return io
+    },
+    getIO: () => {
+        if (!io) {
+            throw new Error('socket.io is not initialised!')
+        }
+        return io
+    }
+}
