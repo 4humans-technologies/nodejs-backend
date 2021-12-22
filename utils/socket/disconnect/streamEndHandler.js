2321d569ecf0dabb7dda891b23384b3b6296081c
Rohit kumar <rohitjadav1729@gmail.com>
2021-12-22 17:39:54 +0530

    fixed bug in on disconnect stream handler, now it's fail safe

--- utils/socket/disconnect/streamEndHandler.js
+++ utils/socket/disconnect/streamEndHandler.js
@@ -32,47 +32,61 @@ module.exports = function onDisconnectStreamEndHandler(client) {
       .lean(),
   ])
     .then(([stream, model]) => {
-      if (
-        stream.status !== "ended" &&
-        model.isStreaming &&
-        model.currentStream
-      ) {
-        /**
-         * if stream was actually ongoing and model was live
-         * then only emit the event
-         */
+      /**
+       * below code is just for analytics purpose
+       */
+      if (stream) {
+        if (stream?.status === "ended") {
+          console.error(
+            "Stream was ended already still, on disconnect handler ran"
+          )
+        }
+      }
+      if (model) {
+        if (model?.isStreaming) {
+          console.error(
+            "Model was not streaming still, on disconnect handler ran"
+          )
+        }
+      }
+      const publicRoom = `${client.streamId}-public`
+      redisClient.del(publicRoom, (err, response) => {
+        if (!err) {
+          console.log("Stream delete redis response:", response)
+          io.getIO().emit(socketEvents.deleteStreamRoom, {
+            modelId: client.data.relatedUserId,
+            liveNow: io.decreaseLiveCount(client.data.relatedUserId),
+          })
 
-        const publicRoom = `${client.streamId}-public`
-        redisClient.del(publicRoom, (err, response) => {
-          if (!err) {
-            console.log("Stream delete redis response:", response)
-            io.getIO().emit(socketEvents.deleteStreamRoom, {
-              modelId: client.data.relatedUserId,
-              liveNow: io.decreaseLiveCount(client.data.relatedUserId),
-            })
-
-            /* destroy the stream chat rooms, 
+          /* destroy the stream chat rooms, 
               adding timeout so that isModelOffline ste can be set the later
               when the "you-left-the-room" event occurs safely get out of the
               room (problem due to setInterval running in loop)
               will definitely look to improve this strategy in future
             */
-            setTimeout(() => {
-              io.getIO().in(publicRoom).socketsLeave(publicRoom)
-            }, 1000)
-          } else {
-            throw err
-          }
-        })
-      }
+          setTimeout(() => {
+            io.getIO().in(publicRoom).socketsLeave(publicRoom)
+          }, 1000)
+        } else {
+          throw err
+        }
+      })
     })
     .catch((err) => {
-      /* may emit to the user */
       /* log that stream was not closed */
       console.error(
         "The streaming status was not updated(closed) Reason: ",
         err.message
       )
+
+      /**
+       * in the event of error also, pull the model out of liveModelsCount
+       */
+
+      io.getIO().emit(socketEvents.deleteStreamRoom, {
+        modelId: client.data.relatedUserId,
+        liveNow: io.decreaseLiveCount(client.data.relatedUserId),
+      })
 
       const publicRoom = `${client.streamId}-public`
       /* see reason written above to find the setTimeout reason */
