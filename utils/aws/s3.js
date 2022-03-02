3e83b968157bdd924d227140a3d7eb696bc1c6dd
Rohit kumar <rohitjadav1729@gmail.com>
2022-03-02 18:58:53 +0530

    add img delete, repair followers

--- utils/aws/s3.js
+++ utils/aws/s3.js
@@ -3,8 +3,12 @@ const { nanoid } = require("nanoid/async")
 
 const region = "ap-south-1"
 const bucketName = "dreamgirl-public-bucket"
-const accessKeyId = process.env.AWS_S3_ACCESS_KEY_ID
-const secretAccessKey = process.env.AWS_S3_SECRET_ACCESS_KEY
+
+// const accessKeyId = process.env.AWS_S3_ACCESS_KEY_ID
+// const secretAccessKey = process.env.AWS_S3_SECRET_ACCESS_KEY
+
+const accessKeyId = "AKIA6BNW6CGZE6SOUW33"
+const secretAccessKey = "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"
 
 const s3 = new aws.S3({
   region: region,
@@ -79,27 +83,44 @@ exports.deleteImage = (url) => {
       Bucket: bucketName,
       key: url.substr(60),
     })
+    .promise()
     .then((result) => {
-      console.info("Delete object: ", url.substr(60))
+      console.info("Delete object: ", url.substr(97))
       return result
     })
+    .catch((e) => console.log(e))
 }
 
 exports.deleteImages = (urls = []) => {
   /**
    * provide the complete url without deleting the initial part
    */
-  return s3.deleteObjects({
-    Bucket: bucketName,
-    Delete: {
-      Objects: urls
-        .filter((key) => {
-          if (!key) {
-            return false
-          }
-          return true
-        })
-        .map((key) => ({ Key: key.substr(60) })),
-    },
-  })
+  return s3
+    .deleteObjects({
+      Bucket: bucketName,
+      Delete: {
+        Objects: urls
+          .filter((key) => {
+            if (!key) {
+              // remove null entries
+              return false
+            }
+            return true
+          })
+          .map((key) => ({ Key: decodeURI(key.substr(60)) })),
+        Quiet: false,
+      },
+    })
+    .promise()
 }
+// const urls = [
+//   "https://dreamgirl-public-bucket.s3.ap-south-1.amazonaws.com/image-album/621da12715854937946fd6d2/Rdf3lIyJQUmqTcj_tzSYPmwV%2A%2A__original.jpeg",
+//   "https://dreamgirl-public-bucket.s3.ap-south-1.amazonaws.com/image-album/621da12715854937946fd6d2/KUJ9nXPKsZqToKPDyKYEZBcH%2A%2A__original.jpeg",
+//   // "https://dreamgirl-public-bucket.s3.ap-south-1.amazonaws.com/ZH59-cQua7ik8f0C3AWjBOU7.jpeg",
+//   "https://dreamgirl-public-bucket.s3.ap-south-1.amazonaws.com/image-album/621da12715854937946fd6d2/Rdf3lIyJQUmqTcj_tzSYPmwV%2A%2A__thumbnail.jpeg",
+//   "https://dreamgirl-public-bucket.s3.ap-south-1.amazonaws.com/image-album/621da12715854937946fd6d2/KUJ9nXPKsZqToKPDyKYEZBcH%2A%2A__thumbnail.jpeg"
+// ]
+
+// this.deleteImages(urls).then((result) => {
+//   console.log(result)
+// })
