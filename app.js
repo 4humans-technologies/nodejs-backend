a03bd709091583583c679e51256e59613bf8c910
rohit kumar <rohitjhadav1729@gmail.com>
2022-06-27 17:26:05 +0530

    modified connect url

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ const adminAuth = require("./routes/ADMIN/ra-admin/auth/auth")
 if (process.env.LOCAL_DB === "false") {
   // these were the creds for Digital ocean database .👇👇
   // var CONNECT_URL = `mongodb+srv://${process.env.DO_MONGO_USER}:${process.env.DO_MONGO_PASS}@dreamgirl-mongodb-3node-blr-1-c5185824.mongo.ondigitalocean.com/${process.env.DO_MONGO_DB_NAME}?authSource=${process.env.DO_MONGO_AUTH_SOURCE}&replicaSet=${process.env.DO_MONGO_REPLICA_SET}&ssl=true`
-  var CONNECT_URL = `mongodb+srv://${process.env.NODE_TODO_MONGO_ATLAS_ROHIT_USER}:${process.env.NODE_TODO_MONGO_ATLAS_ROHIT_PASS}@nodejs.fsqgg.mongodb.net/${process.env.DB_NAME}?w=majority`
+  var CONNECT_URL = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.acfgh.mongodb.net/${process.env.DB_NAME}?w=majority`
 } else {
   // CONNECT_URL = `mongodb://192.168.1.104:27017/${process.env.DB_NAME}`;
   // CONNECT_URL = `mongodb://localhost:27017/${process.env.DB_NAME}`
