797079ed68f83514acf0341579a72d17837eecf3
Shubham Verma <shubham@heady.io>
2022-12-17 23:52:45 +0530

    added new api for admin packages

---
+++ controllers/ADMIN/adminPackageController.js
@@ -0,0 +1,73 @@
+const Package = require("../../models/globals/package");
+
+//create package
+exports.createPackage = async (req, res, next) => {
+  const requestBody = req.body
+
+  try {
+    const packageRecord = await Package.create({
+      "status": "INACTIVE",
+      "INR": requestBody.INR,
+      "coin": requestBody.coin,
+      actualAmountINR: requestBody.actualAmount,
+      discountedAmountINR: requestBody.discountedAmount,
+      packageUrl: requestBody?.packageUrl || "",
+      description: requestBody?.description || ""
+    });
+    return res.status(200).json({
+      actionStatus: "success",
+      data: packageRecord,
+    })
+  } catch (err) {
+    next(err)
+  }
+}
+
+//update package
+exports.updatePackage = async (req, res, next) => {
+  const packageId = req.params.id;
+  const requestBody = req.body;
+  try {
+    await Package.updateOne({
+      _id: packageId,
+    }, {
+      "status": requestBody.status,
+    });
+    return res.status(200).json({
+      actionStatus: "success",
+      message:"package update"
+    })
+  } catch (err) {
+    next(err)
+  }
+}
+
+//list package
+exports.packageList = async (req, res, next) => {
+  const requestBody = req.query;
+  console.log(requestBody)
+
+  try {
+    const packageRecord = await Package.find().lean();
+    return res.status(200).json({
+      actionStatus: "success",
+      data: packageRecord,
+    })
+  } catch (err) {
+    next(err)
+  }
+}
+
+//get package by id
+exports.getPackageById = async (req, res, next) => {
+  const packageId = req.params?.id;
+  try {
+    const packageRecord = await Package.findById(packageId).lean();
+    return res.status(200).json({
+      actionStatus: "success",
+      data: packageRecord,
+    })
+  } catch (err) {
+    next(err)
+  }
+}
---
+++ routes/ADMIN/adminPackageRoutes.js
@@ -0,0 +1,62 @@
+const router = require("express").Router()
+const packageController = require("../../controllers/ADMIN/adminPackageController")
+const {
+  checkForSuperAdminOrStaffWithPermission,
+} = require("../../middlewares/userTypeChecker")
+const tokenVerify = require("../../middlewares/tokenVerify")
+
+const { body } = require("express-validator")
+
+router.post(
+  "/",
+  [body("forCoins").isInt()],
+  tokenVerify,
+//   (req, res, next) => {
+//     if (req.user.permissions.includes("create-package")) {
+//       next()
+//     } else {
+//       const err = new Error(
+//         "Permission denied, you don't have permission to perform this action"
+//       )
+//       err.statusCode = 403
+//       return next(error)
+//     }
+//   },
+packageController.createPackage
+)
+
+router.put(
+    "/:id",
+    [body("status").isString()],
+    tokenVerify,
+    // (req, res, next) => {
+    //   if (req.user.permissions.includes("update-package")) {
+    //     next()
+    //   } else {
+    //     const err = new Error(
+    //       "Permission denied, you don't have permission to perform this action"
+    //     )
+    //     err.statusCode = 403
+    //     return next(error)
+    //   }
+    // },
+    packageController.updatePackage
+  )
+  router.get(
+    "/",
+    tokenVerify,
+    // (req, res, next) => {
+    //   if (req.user.permissions.includes("update-package")) {
+    //     next()
+    //   } else {
+    //     const err = new Error(
+    //       "Permission denied, you don't have permission to perform this action"
+    //     )
+    //     err.statusCode = 403
+    //     return next(error)
+    //   }
+    // },
+    packageController.packageList
+  )
+
+module.exports = router
